cc20d4a489197513ac804c0343d1cc00e7e54fac
Sokratis Vogiatzakis <sokvogiatzakis@gmail.com>
2022-01-26 19:09:10 +0200

    Minor project setting changes.

--- Assets/Scripts/Bootstrap.cs
+++ Assets/Scripts/Bootstrap.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Bootstrap : MonoBehaviour
+namespace Pub
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Bootstrap : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
